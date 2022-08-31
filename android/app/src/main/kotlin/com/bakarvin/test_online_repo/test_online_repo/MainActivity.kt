ba85cd506669a91e9ba3ac7cedff32d61cd1ac4c
bakarvin <arviin.maulana17@gmail.com>
2022-08-31 14:39:39 +0700

    init commit

---
+++ android/app/src/main/kotlin/com/bakarvin/test_online_repo/test_online_repo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bakarvin.test_online_repo.test_online_repo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/components/listtile_template.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class ListTileCustom extends StatelessWidget {
+  final bool isSwitch;
+  final String title;
+  final IconData leadIcon;
+  final IconData trailingIcon;
+  final VoidCallback callback;
+  final Widget widgetCustom;
+  const ListTileCustom({
+    required this.isSwitch,
+    required this.title,
+    required this.leadIcon,
+    required this.trailingIcon,
+    required this.callback,
+    required this.widgetCustom
+  }) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height / 10,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Icon(
+              leadIcon,
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Text(
+              title,
+              style: const TextStyle(
+                color: Colors.green
+              ),
+            ),
+          ),
+          isSwitch == false
+          ? Expanded(
+              flex: 1,
+              child: Icon(
+                trailingIcon,
+                color: Colors.blueAccent,
+              ),
+          )
+          : const Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 15
+                ),
+              ),
+          ),
+        ],
+      ),
+    );
+  }
+}
